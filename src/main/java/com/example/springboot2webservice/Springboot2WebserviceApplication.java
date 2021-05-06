967dbe9340923e55a850e9b9971e6840153f4429
hsw <52780262+DevaCoding@users.noreply.github.com>
2021-05-06 14:04:43 +0900

    게시판 수정, 삭제 화면 구성 및 .gitignore에 구글 클라이언트 id, passwd 등록

--- src/main/java/com/example/springboot2webservice/Springboot2WebserviceApplication.java
+++ src/main/java/com/example/springboot2webservice/Springboot2WebserviceApplication.java
@@ -2,12 +2,13 @@ package com.example.springboot2webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Springboot2WebserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot2WebserviceApplication.class, args);
 	}
-
 }
